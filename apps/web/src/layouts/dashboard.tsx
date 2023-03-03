8de97f86b543af0006f858fb97e2890447cd7446
xencodes <xencodes@icloud.com>
2023-03-03 14:44:07 +0530

    chore: set tiltle

--- apps/web/src/layouts/dashboard.tsx
+++ apps/web/src/layouts/dashboard.tsx
@@ -1,5 +1,6 @@
 import { Box, Flex, Heading, HStack } from '@chakra-ui/react';
 import { Sidebar } from '@/lib/components/sidebar';
+import Head from 'next/head';
 
 type DashboardLayoutProps = {
 	title: string;
@@ -7,28 +8,33 @@ type DashboardLayoutProps = {
 	children: React.ReactNode;
 };
 function DashboardLayout({ title, children, action }: DashboardLayoutProps) {
-	console.log('action', action);
 	const borderColor = '#60606077';
-	return (
-		<Flex h='100vh' overflow='auto'>
-			<Box w='72' borderRightWidth='1px' borderRightColor={borderColor}>
-				<Sidebar />
-			</Box>
-			<Box flexGrow={1}>
-				<HStack
-					p={4}
-					spacing={6}
-					borderBottomWidth='1px'
-					borderBottomColor={borderColor}
-					height='60px'
-				>
-					<Heading size='md'>{title}</Heading>
-					<Box>{action}</Box>
-				</HStack>
 
-				<Box p={4}>{children}</Box>
-			</Box>
-		</Flex>
+	return (
+		<>
+			<Head>
+				<title>{title} | BotMate</title>
+			</Head>
+			<Flex h='100vh' overflow='auto'>
+				<Box w='72' borderRightWidth='1px' borderRightColor={borderColor}>
+					<Sidebar />
+				</Box>
+				<Box flexGrow={1}>
+					<HStack
+						p={4}
+						spacing={6}
+						borderBottomWidth='1px'
+						borderBottomColor={borderColor}
+						height='60px'
+					>
+						<Heading size='md'>{title}</Heading>
+						<Box>{action}</Box>
+					</HStack>
+
+					<Box p={4}>{children}</Box>
+				</Box>
+			</Flex>
+		</>
 	);
 }
 
