2d85992d1ac48df387d74762db8bf3fe200e9a1d
xencodes <xencodes@icloud.com>
2023-03-04 23:09:56 +0530

    chore: update message type

--- apps/web/src/libs/components/moderation/filters/message-types.tsx
+++ apps/web/src/libs/components/moderation/filters/message-types.tsx
@@ -1,7 +1,39 @@
-import { Card } from '@/libs/ui';
-import { Stack, HStack, Spacer, Text } from '@chakra-ui/react';
+import { Card, RadioButton } from '@/libs/ui';
+import {
+  Stack,
+  HStack,
+  Spacer,
+  Text,
+  useDisclosure,
+  Modal,
+  Button,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  ButtonGroup,
+  Input,
+  Switch,
+  FormControl,
+  FormHelperText,
+} from '@chakra-ui/react';
+import { useState } from 'react';
 
-const MessageTypes = [
+// todo: make this a component and reusable
+type MessageTypesOption = {
+  label: string;
+  type: 'checkbox' | 'input';
+  yes?: MessageTypesOption;
+};
+type MessageTypesType = {
+  label: string;
+  exceptionExample?: string;
+  options?: {
+    [key: string]: MessageTypesOption;
+  };
+};
+const MessageTypes: MessageTypesType[] = [
   {
     label: 'Links',
     exceptionExample: 'www.domain.com',
@@ -19,13 +51,26 @@ const MessageTypes = [
       limit_file_size: {
         label: 'Limit file size',
         type: 'checkbox',
+        yes: {
+          label: 'File size',
+          type: 'input',
+        },
       },
     },
+  },
+  {
+    label: 'Stickers',
+    exceptionExample: 't.me/addstickers/...',
   },
 ];
 
 type FilterMessageTypesProps = {};
 function FilterMessageTypes({}: FilterMessageTypesProps) {
+  const d = useDisclosure();
+  const [activeMsgType, setActiveMsgType] = useState<MessageTypesType | null>(
+    null,
+  );
+
   return (
     <Card
       title="Message Type"
@@ -33,9 +78,9 @@ function FilterMessageTypes({}: FilterMessageTypesProps) {
       minW="300px"
     >
       <Stack>
-        {MessageTypes.map(({ label, exceptionExample }) => (
-          <HStack key={label}>
-            <Text size="sm">{label}</Text>
+        {MessageTypes.map((type) => (
+          <HStack key={type.label}>
+            <Text size="sm">{type.label}</Text>
             <Spacer />
             <Text
               userSelect="none"
@@ -45,12 +90,60 @@ function FilterMessageTypes({}: FilterMessageTypesProps) {
                 opacity: 0.8,
               }}
               transition="opacity 0.2s"
+              onClick={() => {
+                setActiveMsgType(type);
+                d.onOpen();
+              }}
             >
               Allow all
             </Text>
           </HStack>
         ))}
       </Stack>
+
+      <Modal
+        isOpen={d.isOpen}
+        onClose={d.onClose}
+        trapFocus={false}
+        closeOnOverlayClick={false}
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>{activeMsgType?.label}</ModalHeader>
+          <ModalBody>
+            <Stack spacing={4}>
+              <RadioButton options={['Allow', 'Block']} />
+              <FormControl>
+                <Input placeholder="Enter your filter" />
+                {activeMsgType?.exceptionExample && (
+                  <FormHelperText>
+                    Example: <b>{activeMsgType.exceptionExample}</b>
+                  </FormHelperText>
+                )}
+              </FormControl>
+              {/* todo: make it a reusable component, and support nested values */}
+              {activeMsgType?.options &&
+                Object.entries(activeMsgType.options).map(([key, value]) => (
+                  <HStack key={key}>
+                    <Text>{value.label}</Text>
+                    <Spacer />
+                    {value.type === 'checkbox' ? (
+                      <Switch />
+                    ) : (
+                      <Input placeholder="Enter value" />
+                    )}
+                  </HStack>
+                ))}
+            </Stack>
+          </ModalBody>
+
+          <ModalFooter>
+            <ButtonGroup>
+              <Button onClick={d.onClose}>Close</Button>
+            </ButtonGroup>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Card>
   );
 }
