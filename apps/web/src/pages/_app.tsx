f4e67c1e609d075b7a1d61e1b7991d13485fc947
xencodes <xencodes@icloud.com>
2023-03-05 20:13:17 +0530

    chore(frontend): remove graphql

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -1,12 +1,5 @@
 import { NextPage } from 'next';
 import { AppProps } from 'next/app';
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
 import { ChakraProvider } from '@chakra-ui/react';
 import { AnimatePresence } from 'framer-motion';
 import { theme } from '@botmate/theme';
@@ -14,27 +7,6 @@ import { theme } from '@botmate/theme';
 import '../styles/globals.css';
 import '@fontsource/inter/400.css';
 import '@fontsource/open-sans/700.css';
-
-const httpLink = createHttpLink({
-  uri: 'http://localhost:8080/graphql',
-});
-
-const authLink = setContext((_, { headers }) => {
-  // todo: get token from cookie
-  const token = localStorage.getItem('token');
-
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
-});
-
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
 
 type NextPageWithLayout = NextPage & {
   getLayout?: (page: React.ReactElement) => React.ReactNode;
@@ -49,9 +21,7 @@ function BotMate({ Component, pageProps }: AppPropsWithLayout) {
 
   return (
     <AnimatePresence>
-      <ApolloProvider client={client}>
-        <ChakraProvider theme={theme}>{layout}</ChakraProvider>
-      </ApolloProvider>
+      <ChakraProvider theme={theme}>{layout}</ChakraProvider>
     </AnimatePresence>
   );
 }
