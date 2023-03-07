21b64340fa0ef6be20cfb39ead8a55534f1f5301
xencodes <xencodes@icloud.com>
2023-03-07 18:12:41 +0530

    fix: api path

--- apps/web/openapi-config.ts
+++ apps/web/openapi-config.ts
@@ -2,7 +2,7 @@ import type { ConfigFile } from '@rtk-query/codegen-openapi';
 
 const config: ConfigFile = {
   schemaFile: 'http://localhost:8080/api/docs-json',
-  apiFile: './src/libs/redux/base-query.ts',
+  apiFile: './src/libs/store/base-query.ts',
   apiImport: 'apiSlice',
   outputFile: './src/libs/api.ts',
   exportName: 'userApi',
