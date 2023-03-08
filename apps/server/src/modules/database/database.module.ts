f04f5dc111f31b5a7e867ba8957c13f866a8a818
xencodes <xencodes@icloud.com>
2023-03-08 13:13:02 +0530

    chore: add support for sqlite3

--- apps/server/src/modules/database/database.module.ts
+++ apps/server/src/modules/database/database.module.ts
@@ -9,16 +9,27 @@ import { Bot } from '@/entities/bot.entity';
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: +configService.get('DB_PORT'),
-        database: configService.get('DB_NAME'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        entities: [User, Bot],
-        synchronize: true,
-      }),
+      useFactory: (configService: ConfigService) => {
+        if (configService.get('DB_TYPE') === 'postgres') {
+          return {
+            type: 'postgres',
+            host: configService.get('DB_HOST'),
+            port: +configService.get('DB_PORT'),
+            database: configService.get('DB_NAME'),
+            username: configService.get('DB_USERNAME'),
+            password: configService.get('DB_PASSWORD'),
+            entities: [User, Bot],
+            synchronize: true,
+          };
+        }
+
+        return {
+          type: 'sqlite',
+          database: 'database.sqlite',
+          entities: [User, Bot],
+          synchronize: true,
+        };
+      },
       inject: [ConfigService],
     }),
   ],
