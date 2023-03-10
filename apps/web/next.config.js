f1d2d822b75c38073a7bbb226a4e7fe263e58d82
xencodes <xencodes@icloud.com>
2023-03-10 11:53:51 +0530

    fix: remove redirects in `next.config.js`

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -3,16 +3,6 @@ const removeImports = require('next-remove-imports')();
 module.exports = removeImports({
   reactStrictMode: true,
   transpilePackages: ['ui'],
-  output: 'export',
-  async redirects() {
-    return [
-      {
-        source: '/settings',
-        destination: '/settings/general',
-        permanent: true,
-      },
-    ];
-  },
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
