8ba52d286e541e30317bc0f3243a1485ac56f8b5
xencodes <xencodes@icloud.com>
2023-03-14 10:02:09 +0530

    fix: incorrect botmate version

--- apps/server/src/modules/botmate/botmate.service.ts
+++ apps/server/src/modules/botmate/botmate.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@nestjs/common';
-import findUp = require('find-up');
-import { readFileSync } from 'fs';
+import { existsSync, readFileSync } from 'fs';
+import * as path from 'path';
 
 @Injectable()
 export class BotMateService {
   async getVersion() {
-    const packageJsonPath = await findUp('package.json');
-    if (packageJsonPath) {
-      const packageJson = readFileSync(packageJsonPath, 'utf8');
-      return JSON.parse(packageJson).version;
+    let version = '0.0.0';
+    const packagePath = path.join(__dirname, '../..', 'package.json');
+
+    if (existsSync(packagePath)) {
+      const packageJson = JSON.parse(readFileSync(packagePath, 'utf8'));
+
+      version = packageJson.version;
     }
+
+    return version;
   }
 
   update() {
