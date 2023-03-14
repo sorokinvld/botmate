376fb56163ea968f3f804a0ee254645e0d2890ee
xencodes <xencodes@icloud.com>
2023-03-14 07:12:49 +0530

    fix: service message initial checks

--- apps/web/src/features/moderation/filters/service-messages.tsx
+++ apps/web/src/features/moderation/filters/service-messages.tsx
@@ -8,6 +8,8 @@ function FilterServiceMessages({}: FilterServiceMessagesProps) {
     type: 'service_messages',
   });
 
+  const { new_chat_members, left_chat_member, pinned_message } = form.watch();
+
   return (
     <Card
       title="Service Messages"
@@ -22,17 +24,26 @@ function FilterServiceMessages({}: FilterServiceMessagesProps) {
         <HStack>
           <Text size="sm">New members</Text>
           <Spacer />
-          <Switch {...form.register('new_chat_members')} />
+          <Switch
+            isChecked={new_chat_members}
+            {...form.register('new_chat_members')}
+          />
         </HStack>
         <HStack>
           <Text size="sm">Left members</Text>
           <Spacer />
-          <Switch {...form.register('left_chat_member')} />
+          <Switch
+            isChecked={left_chat_member}
+            {...form.register('left_chat_member')}
+          />
         </HStack>
         <HStack>
           <Text size="sm">Pinned message</Text>
           <Spacer />
-          <Switch {...form.register('pinned_message')} />
+          <Switch
+            isChecked={pinned_message}
+            {...form.register('pinned_message')}
+          />
         </HStack>
       </Stack>
     </Card>
