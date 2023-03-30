1464d606445480a2e9b855e0bd7ac40d596bef18
xencodes <xencodes@icloud.com>
2023-03-30 13:11:55 +0530

    fix: cached

--- packages/core/helper-plugin/lib/contexts/botMateApp.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.botMateAppContext = void 0;
-const react_1 = require("react");
-const botMateAppContext = (0, react_1.createContext)({});
-exports.botMateAppContext = botMateAppContext;
-//# sourceMappingURL=botMateApp.js.map
--- packages/core/helper-plugin/lib/contexts/bots.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.botsContext = void 0;
-const react_1 = require("react");
-const botsContext = (0, react_1.createContext)({});
-exports.botsContext = botsContext;
-//# sourceMappingURL=bots.js.map
